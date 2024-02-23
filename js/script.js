8c39a1a69e4a0c38295450d548ac2dd910839088
alinapopok <alinapopok1@gmail.com>
2024-02-23 17:56:16 +0200

    hw-7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+//Task #1
+
+const name = 'John';
+const admin = name;
+alert(admin)
+
+//Task #2
+
+const planetName = 'Earth';
+const userName = 'Alina';
+
+//Task #3
+
+const foo = '10' + 10;
+console.log(typeof foo);
