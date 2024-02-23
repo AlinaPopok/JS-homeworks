e1220855ac1c760131bbf7d37d4813fecc4aea00
alinapopok <alinapopok1@gmail.com>
2024-02-23 17:35:26 +0200

    hw-6

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 'use strict';
+//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+const string1 = prompt('enter something');
+const string2 = prompt('enter something');
+const string3 = prompt('enter something');
 
-const userName = prompt('Enter your name');
+console.log (`${string1} ${string2} ${string3}`)
 
-alert(`Hello, ${userName}! How are you?`);
+//Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+
+const num = 12345;
+const str = String(num);
+const num2 = str.split('').join(' ');
+console.log(num2)
