dc06d024c8f04579611cf07219ecf17956eab9d6
alinapopok <alinapopok1@gmail.com>
2024-02-28 14:23:24 +0200

    hw-9

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const tempValue = +prompt('Введіть градуси');
+const scale = prompt('Оберіть шкалу "C" чи "F"');
+
+if( scale === 'C' || scale === 'c'){
+
+    alert(`По Фаренгейту це буде ${(tempValue * 9/5) + 32} F`);
+}
+else if ( scale === 'F' || scale === 'f'){
+    alert(`По Цельсію це буде ${Math.round((tempValue - 32) * 5/9)} C`);
+}
