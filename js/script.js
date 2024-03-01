b81c3ac247d3360876c50784086f636c3ef3a7de
alinapopok <alinapopok1@gmail.com>
2024-03-01 15:58:10 +0200

    hw-11

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const num1 = +prompt('Введіть перше число');
+const num2 = +prompt('Введіть  друге число');
+const operation = prompt('Введіть математичну операцію "add", "sub", "mult", "div"');
+
+
+
+if( !isNaN(num1) && !isNaN(num2) ){
+    switch (operation.toLowerCase()){
+        case 'add':
+            alert(`${num1} + ${num2} = ${num1 + num2}`)
+            break
+        case 'sub':
+            alert(`${num1} - ${num2} = ${num1 - num2}`)
+            break
+        case 'div':
+            alert(`${num1} / ${num2} = ${num1 / num2}`)
+            break
+        case 'mult':
+            alert(`${num1} * ${num2} = ${num1 * num2}`)
+            break
+        default : alert('Оберіть одну з операцій')
+    }
+}
+else alert('Введіть числове значення')
