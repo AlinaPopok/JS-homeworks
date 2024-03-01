ac46d3c7d51cfd46a3da9afefea94af29d28a051
alinapopok <alinapopok1@gmail.com>
2024-03-01 13:18:23 +0200

    hw-10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+
+console.log(`Result: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Result: ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`Result: ${num1} / ${num2} = ${num1 / num2}`);
+console.log(`Result: ${num1} - ${num2} = ${num1 - num2}`);
+
