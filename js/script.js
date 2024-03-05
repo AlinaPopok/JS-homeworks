2be2483cd863ecb68ba890b28a6b3fe2e8c7bdc7
alinapopok <alinapopok1@gmail.com>
2024-03-05 13:19:06 +0200

    hw-16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const num1 = prompt('Введіть перше число');
+const num2 = prompt('Введіть друге число');
+const num3 = prompt('Введіть третє число');
+alert((Number(num1) + Number(num2) + Number(num3)) / 3)
