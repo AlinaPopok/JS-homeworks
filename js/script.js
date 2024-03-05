9a340365f97e0445d0f31e8f9cb32ac63b331e8c
alinapopok <alinapopok1@gmail.com>
2024-03-05 13:49:39 +0200

    hw-14

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 'use strict';
+const num1 = prompt('Ведіть перше число');
+const num2 = prompt('Ведіть друге число');
+alert(`ви ввели ${Number(num1)} і ${Number(num2)} \n ${Number(num1)} + ${Number(num2)} = ${Number(num1) + Number(num2)} \n ${Number(num1)} - ${Number(num2)} = ${Number(num1) - Number(num2)} \n ${Number(num1)} * ${Number(num2)} = ${Number(num1) * Number(num2)} \n ${Number(num1)} / ${Number(num2)} = ${Number(num1) / Number(num2)}`)
