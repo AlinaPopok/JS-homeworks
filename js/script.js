f1cac0e63c83b3bbfaf6f06f741bff16889044a6
alinapopok <alinapopok1@gmail.com>
2024-03-11 15:01:42 +0200

    hw-19

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const num = (prompt('Введіть число'));
+
+if (isNaN(Number(num))) {
+    alert('Введіть коректне число')
+} else {
+    if (num.charAt(num.length - 1) === '1') {
+        alert(`Вам ${num} рік`)
+    } else if (num.charAt(num.length - 1) === '2'
+        || num.charAt(num.length - 1) === '3'
+        || num.charAt(num.length - 1) === '4') {
+        alert(`Вам ${num} роки`)
+    } else {
+        alert(`Вам ${num} років`)
+    }
+}
