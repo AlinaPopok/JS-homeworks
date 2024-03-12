16de7a01ac04426071583e9641d1067ca29b0aca
alinapopok <alinapopok1@gmail.com>
2024-03-12 15:36:18 +0200

    hw-24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let result = 0;
+let result2 = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+    result2 += Math.pow(arr[i], 2)
+}
+
+console.log(`Сумма елементів массиву = ${result}`)
+console.log(`Сумма квадратів елементів массиву = ${result2}`)
