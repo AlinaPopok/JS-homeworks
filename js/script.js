0d03921ed9bc0a5f2a3e758b7d0735595466f520
alinapopok <alinapopok1@gmail.com>
2024-03-13 12:32:40 +0200

    hw-25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const num = prompt('Введіть число');
+let divider = 1;
+
+if (Number(num) < 1) {
+    console.log(NaN)
+} else if (Number(num) === 1) {
+    console.log(`Найменшим дільником числа ${num} є число ${divider}`)
+} else {
+    for (let i = 2; i <= Number(num); i++) {
+        if (Number(num) % i === 0) {
+            divider = i;
+            console.log(`Найменшим дільником числа ${num} є число ${divider}`)
+            break
+        }
+    }
+}
+
+
