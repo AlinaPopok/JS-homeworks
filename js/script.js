178a01a0f99f7fdff37398e30e9199f30a7e6483
alinapopok <alinapopok1@gmail.com>
2024-03-14 14:07:05 +0200

    hw-26

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let result = 0;
+
+loop1: for (let i = 1; i < 21; i++) {
+    if (i % 2 === 0) {
+        continue loop1;
+    }
+    result += i;
+}
+
+console.log(`сума всіх непарних чисел в діапазоні від 1 до 20 = ${result}`)
