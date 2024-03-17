60fef5fb0a7a6be2d2eae6eace3023043b0292d3
alinapopok <alinapopok1@gmail.com>
2024-03-17 16:52:55 +0200

    hw-28

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+const num = prompt('Введіть число');
+const power = prompt('Введіть степінь');
+
+const numToPower = function (num, power = 1) {
+    const result = num ** power;
+    return isNaN(result) ? 'помилка' : result
+};
+
+alert(numToPower(num, power));
