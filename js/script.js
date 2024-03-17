e95e81054ce2f3913d82b97fd0fd913c96fb93a7
alinapopok <alinapopok1@gmail.com>
2024-03-17 15:04:07 +0200

    hw-27

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const random = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const key = function (length, characters) {
+    let result = ''
+    for (let i = 0; i < length; i++) {
+        result += characters[random(0, characters.length)];
+    }
+    return result;
+}
+
+console.log(key(10, characters))
