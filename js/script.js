74af89c222b0869bb77e09b4bce563c887b2f226
alinapopok <alinapopok1@gmail.com>
2024-03-18 11:57:40 +0200

    hw-29

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const padString = function (str, strLength, symbol = '*', addRight = true) {
+
+    if (typeof str !== 'string') {
+        return 'Помилка! Перший аргумент має бути string'
+    }
+    if (typeof strLength !== 'number') {
+        return 'Помилка! Другий аргумент має бути number'
+    }
+    if (typeof symbol !== 'string') {
+        return 'Помилка! Третій аргумент має бути string'
+    }
+    if (typeof addRight !== 'boolean') {
+        return 'Помилка! Четвертий аргумент має бути boolean'
+    }
+    const subStr = str.substring(0, strLength);
+
+    return addRight
+        ? subStr.padEnd(strLength, symbol)
+        : subStr.padStart(strLength, symbol);
+}
+
+console.log(padString('900', 26))
