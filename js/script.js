f3a9d074d4617f1faaa8567e0c40a3b5fcec229c
alinapopok <alinapopok1@gmail.com>
2024-03-18 15:04:42 +0200

    hw-30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const positiveArr = function (arr) {
+    const exampleArr = [];
+    if (arr.length === 0) {
+        return 'Заповніть массив'
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 0) {
+            exampleArr.push(arr[i])
+        }
+    }
+    return exampleArr.length > 0 ? exampleArr : null
+}
+console.log(positiveArr(arr));
