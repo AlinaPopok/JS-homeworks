d8d991f8a14d79eefd80b91afd470dae68b23f63
alinapopok <alinapopok1@gmail.com>
2024-04-09 22:52:04 +0300

    hw-33 to main

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
-"use strict";
-
 const sum = function (a, b) {
-    return a + b;
+  return a + b;
 };
 const div = function (a, b) {
-    return a - b;
+  return a - b;
 };
 
 const mult = function (a, b) {
-    return a * b;
+  return a * b;
 };
 
-sum(5, 5)
-div(5, 5)
-mult(5, 5)
+sum(5, 5);
+div(5, 5);
+mult(5, 5);
