b05973d0a31352b13add5752094fa12bc859e7fa
alinapopok <alinapopok1@gmail.com>
2024-04-15 18:10:44 +0300

    hw-52

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
+// Task #1
+const getFactorial = function (num) {
+  if (num === 1 || num === 0) return 1;
+  return num * getFactorial(num - 1);
+};
+console.log(getFactorial(3));
+
+// Task #2
+const pow = function (num, degree) {
+  if (degree === 1) return num;
+  return num * pow(num, degree - 1);
+};
+console.log(pow(3, 3));
+
+// Task #3
 const sum = function (a, b) {
-  return a + b;
+  if (b === 0) return a;
+  return sum(++a, --b);
 };
-const div = function (a, b) {
-  return a - b;
-};
-
-const mult = function (a, b) {
-  return a * b;
-};
-
-sum(5, 5);
-div(5, 5);
-mult(5, 5);
+console.log(sum(3, 3));
